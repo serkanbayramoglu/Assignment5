4b19a049f7f843a3154552423141c3de957868bb
Serkan Bayramoglu <bayramoglu.serkan@gmail.com>
2022-01-20 14:55:34 +0300

    prepared login screen, login controller, mvc controller, Admin pojo, and admin repository, and a blank calculator page

--- src/main/java/com/simplecalculator/controller/loginController.java
+++ src/main/java/com/simplecalculator/controller/loginController.java
@@ -1,14 +1,47 @@
 package com.simplecalculator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.simplecalculator.entity.Admin;
+import com.simplecalculator.repository.AdminRepository;
+
 
 
 @Controller
 public class loginController {
 	
+	@Autowired
+	AdminRepository adminRepository;
+	
 	@RequestMapping("/")
 	public String showHome() {
 		return "loginScreen";
+	}
+	
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public String isValidUser(@RequestParam("username") String username, @RequestParam("password") String password,
+			ModelMap map) {
+		String page = "loginScreen";
+
+		Admin data = adminRepository.findByUsername(username);
+
+
+		if (password.equals(data.getPassword())) {
+						
+			map.addAttribute("firstname", data.getFirstname());
+			map.addAttribute("lastname", data.getLastname());
+			map.addAttribute("username", username);
+			map.addAttribute("password", password);
+			page = "calculatorScreen";
+		} else {
+			map.addAttribute("errorMessage", "Invalid login credentials");
+		}
+		return page;
 	}
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into admin(id,username,password,firstname,lastname) values (10001,'user1','pass1','John','Smith');
+insert into admin(id,username,password,firstname,lastname) values (10002,'user2','pass2','Mike','Bunny');
