0671c1ac9269e34aa58bda13fa4e462a787f5631
Serkan Bayramoglu <bayramoglu.serkan@gmail.com>
2022-01-20 15:47:09 +0300

    reverted some code

--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-insert into admin(id,username,password,firstname,lastname) values (10001,'user1','pass1','John','Smith');
-insert into admin(id,username,password,firstname,lastname) values (10002,'user2','pass2','Mike','Bunny');
